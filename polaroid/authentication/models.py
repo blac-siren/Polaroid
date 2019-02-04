9727e3f918244ba0e83d9c405e8236be57f55afd
blac-siren <husnein.zac@gmail.com>
2019-02-04 22:16:10 +0300

    [feature] create a user model

--- polaroid/authentication/models.py
+++ polaroid/authentication/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+from django.db. import models
+import django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin, TimestampedModel,)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves user with given usernames, email, password
+        """
+        if not username:
+            raise ValueError("User must have username")
+
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password')
+
+        user = self.create_user(username, email, password)
+        user.is_super = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.Boolean(default=False)
+    is_staff = models.Boolean(default=False)
+
+    USERNAME = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
