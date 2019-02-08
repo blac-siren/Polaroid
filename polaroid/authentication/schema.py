bf89aab8bc400ef1508ee03369f7f79b14d2bfa6
blac-siren <husnein.zac@gmail.com>
2019-02-08 11:05:59 +0300

    [feature] update schema

--- polaroid/authentication/schema.py
+++ polaroid/authentication/schema.py
@@ -3,41 +3,30 @@ from graphene_django.types import DjangoObjectType
 from .models import User
 
 
-class UserNode(DjangoObjectType):
+class UserType(DjangoObjectType):
     class Meta:
         model = User
-        filter_fields = {
-            'username': ['exact', 'istartswith'],
-            'email': ['exact'],
-            'password': ['exact', 'icontains', 'istartswith'],
-        }
-        interfaces = (graphene.relay.Node, )
 
 
-# class UserCreateInput(graphene.InputObjectType):
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
 
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
 
-class UserCreate(graphene.relay.ClientIDMutation):
-    class Input:
-        username = graphene.String()
-        email = graphene.String()
-        password = graphene.String()
-
-    new_user = graphene.Field(UserNode)
-
-    @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        user = User.objects.create_user(**input)
-        user.save
-        return cls(new_user=user)
-
+    def mutate(self, info, **kwargs):
+        new_user = User.objects.create_user(**kwargs)
+        return CreateUser(user=new_user)
 
 class Query(graphene.ObjectType):
-    user = graphene.List(UserNode)
+
+    users = graphene.List(UserType)
 
     def resolve_users(self, info, **kwargs):
         return User.objects.all()
 
 
 class Mutation(graphene.ObjectType):
-    create_user = UserCreate.Field()
+    create_user = CreateUser.Field()
